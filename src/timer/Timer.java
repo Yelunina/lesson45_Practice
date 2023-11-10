468888e994de84c606162eb5019a30f4df0d39de
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-11-10 20:09:19 +0100

    commit

---
+++ src/timer/Timer.java
@@ -0,0 +1,19 @@
+package timer;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Scanner;
+
+
+public interface Timer {
+
+    void startTimer(long seconds);
+
+    long stopWatch(Scanner scanner);
+
+    LocalTime stopWatchInTime(Scanner scanner);
+
+    void alarm(LocalDateTime timeToWakeUp);
+
+    void pomidoroTimer();
+}
